9174f33cf3ad6e0fbc7d4db91d9dfb7d42bf9901
Uwe L. Korn <xhochy@users.noreply.github.com>
2022-03-04 17:10:50 +0100

    Update recipe/test_libtensorflow_cc.sh
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>

--- recipe/test_libtensorflow_cc.sh
+++ recipe/test_libtensorflow_cc.sh
@@ -3,7 +3,7 @@
 set -exuo pipefail
 
 if [[ "${target_platform}" == linux-* ]]; then
-  export LD_FLAGS="${LDFLAGS} -lrt"
+  export LDFLAGS="${LDFLAGS} -lrt"
 fi
 ${CXX} ${CXXFLAGS} ${LDFLAGS} -o test_cc -ltensorflow_cc -ltensorflow_framework test_cc.cc
 ./test_cc
