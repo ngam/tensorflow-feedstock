8d2202c4c5ae3e2664853ae64f5a5140c1c71a4e
Uwe L. Korn <uwe.korn@quantco.com>
2022-03-04 08:54:27 +0100

    Only link librt on Linux

--- recipe/test_libtensorflow_cc.sh
+++ recipe/test_libtensorflow_cc.sh
@@ -2,5 +2,8 @@
 
 set -exuo pipefail
 
-${CXX} ${CXXFLAGS} ${LDFLAGS} -o test_cc -ltensorflow_cc -ltensorflow_framework -lrt test_cc.cc
+if [[ "${target_platform}" == linux-* ]]; then
+  export LD_FLAGS="${LDFLAGS} -lrt"
+fi
+${CXX} ${CXXFLAGS} ${LDFLAGS} -o test_cc -ltensorflow_cc -ltensorflow_framework test_cc.cc
 ./test_cc
