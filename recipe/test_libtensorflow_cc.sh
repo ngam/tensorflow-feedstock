c0309f98edd75bce39ef1df43c24390ec2e27628
Uwe L. Korn <uwe.korn@quantco.com>
2022-03-19 14:26:24 +0000

    Fix next test

--- recipe/test_libtensorflow_cc.sh
+++ recipe/test_libtensorflow_cc.sh
@@ -5,5 +5,5 @@ set -exuo pipefail
 if [[ "${target_platform}" == linux-* ]]; then
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
-${CXX} ${CXXFLAGS} ${LDFLAGS} -o test_cc -ltensorflow_cc -ltensorflow_framework test_cc.cc
+${CXX} ${CXXFLAGS} ${LDFLAGS} -o test_cc test_cc.cc -ltensorflow_cc -ltensorflow_framework
 ./test_cc
